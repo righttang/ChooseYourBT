eed22741adbd168881473873fa2efc2428349877
righttang <righttang15@gmail.com>
2015-02-04 21:19:06 +1100

    Add Douban Registration

--- douban/doubanClient.js
+++ douban/doubanClient.js
@@ -6,24 +6,34 @@ var DoubanClient = require('douban-client');
 //Database Connection
 var mongo = require('mongodb');
 var monk = require('monk');
+var q = require('q');
 var db = monk('localhost:27017/ChooseYourBT');
-var collectionName = 'config';
+var configCollection = 'config';
 
 API_KEY = '08c8a7a3a2be10791a59f6eae83a316b';
 API_SECRET = 'd72371280aaebcff';
 SCOPE = 'movie_basic,movie_basic_r,movie_basic_w';
-your_redirect_uri = 'http://localhost:3000/';
+your_redirect_uri = 'http://localhost:3000/douban';
 
 /*doubanToken = { access_token: '74a6150775b5eedd5df23f166121cc97',
-    douban_user_name: 'TT',
-    douban_user_id: '120997005',
-    expires_in: 604800,
-    refresh_token: '52a48c5adc17ee6cf7eda5109447cb44' };*/
+ douban_user_name: 'TT',
+ douban_user_id: '120997005',
+ expires_in: 604800,
+ refresh_token: '52a48c5adc17ee6cf7eda5109447cb44' };
+ storeDoubanToken();*/
 
 //Get the Douban Client
-exports.getDoubanClient = function () {
+exports.getDoubanClient = function (code) {
+    var deferred = q.defer();
     var client = new DoubanClient(API_KEY, API_SECRET, your_redirect_uri, SCOPE);
-    if (getDoubanTokenFromDB()) {
+    if (code) {
+        client.auth_with_code(code, function (err, doubanToken) {
+            console.log('this is your ' + doubanToken);
+            storeDoubanToken(doubanToken);
+            deferred.resolve(client);
+        });
+    }
+    getDoubanTokenFromDB().then(function (doubanToken) {
         client.loadFromDoubanToken(doubanToken);
         var event = client.movie.search('采访.The.Interview.2014');
         event.on('data', function (err, data) {
@@ -33,38 +43,36 @@ exports.getDoubanClient = function () {
                 console.log(err);
             }
         });
-        return client;
-    } else {
+        deferred.resolve(client);
+    }, function () {
         process.stdout.write(client.authorize_url() + '\n');
-        code = '1a0b45e2ad790aa3';
-        client.auth_with_code(code, function (err, doubanToken) {
-            console.log('this is your ' + doubanToken);
-            var event = client.movie.get('1297192');
-            event.on('data', function (err, data) {
-                console.log(data);
-                if (!err) {
-                    console.log(data);
-                }
-            });
-        });
-    }
+        deferred.reject(client);
+    });
+
+    return deferred.promise;
 };
 
 function getDoubanTokenFromDB() {
-    var collection = db.get(collectionName);
+    var deferred = q.defer();
+    var collection = db.get(configCollection);
     collection.find({ douban_user_name: { $exists: true } }, {},
         function (e, doc) {
             if (doc[0]) {
-                return doc[0];
+                deferred.resolve(doc[0]);
             } else {
-                return null;
+                deferred.reject(null);
             }
         });
-    /*    var promise = collection.update(
-     {douban_user_name: {$exists: true}},
-     {$set: doubanToken},
-     {upsert: true}
-     );*/
+    return deferred.promise;
+}
+
+function storeDoubanToken(doubanToken) {
+    var collection = db.get(configCollection);
+    var promise = collection.update(
+        {douban_user_name: {$exists: true}},
+        {$set: doubanToken},
+        {upsert: true}
+    );
 }
 
 
--- routes/douban.js
+++ routes/douban.js
@@ -12,13 +12,22 @@ var db = monk('localhost:27017/ChooseYourBT');
 var collectionName = 'movie';
 
 router.get('/', function (req, res, next) {
+
+    if (req.query.code) {
+        doubanClient.getDoubanClient(req.query.code);
+    }
+
     queryDataFromDouban('采访.The.Interview.2014').then(function (data) {
-        res.json(data);
+        res.json(JSON.parse(data));
+    }, function (authorize_url) {
+        console.log("Rejectted from second Level");
+        res.writeHead(301,
+            {Location: authorize_url}
+        );
+        res.end();
     });
 
 });
-
-
 
 
 //Do A search Query to Douban
@@ -27,16 +36,22 @@ function queryDataFromDouban(movieName) {
     if (settings.useMock) {
         deferred.resolve(mockdbcollection.getDoubanMock());
     } else {
-        var client = doubanClient.getDoubanClient();
-        var event = client.movie.search(movieName);
-        event.on('data', function (err, data) {
-            if (!err) {
-                deferred.resolve(data);
-            } else {
-                console.log(err);
-                deferred.reject(err);
-            }
-        });
+        doubanClient.getDoubanClient()
+            .then(function (client) {
+                //Success
+                var event = client.movie.search(movieName);
+                event.on('data', function (err, data) {
+                    if (!err) {
+                        deferred.resolve(JSON.parse(data));
+                    } else {
+                        console.log(err);
+                        deferred.reject(err);
+                    }
+                });
+            }, function (client) {
+                console.log("Rejectted from first Level");
+                deferred.reject(client.authorize_url());
+            });
     }
     return deferred.promise;
 
